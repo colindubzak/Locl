9ca35ca9442bbbca0077188f54924260cdf6604d
Joshua Hodgson <114022342+manlikejosh@users.noreply.github.com>
2025-02-02 17:09:51 -0600

    boilerplate

---
+++ Locl/Locl/LoclApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoclApp.swift
+//  Locl
+//
+//  Created by Joshua Hodgson on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LoclApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Locl/LoclTests/LoclTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoclTests.swift
+//  LoclTests
+//
+//  Created by Joshua Hodgson on 2/2/25.
+//
+
+import Testing
+@testable import Locl
+
+struct LoclTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
